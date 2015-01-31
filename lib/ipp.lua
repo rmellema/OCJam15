e1ecd7f662dd1252bb41cff8aa2da8a5b8482484
rmellema <rene.mellema@gmail.com>
2015-01-31 15:16:38 +0100

    Added a simple tsp library

---
+++ lib/ipp.lua
@@ -0,0 +1,48 @@
+local ipp = {}
+
+local function manhattan(o, t)
+  local sum = 0
+  for k, _ in pairs(o) do
+    sum = sum + math.abs(o[k] -t[k])
+  end
+  return sum
+end
+
+local function length(p)
+  local sum = 0
+  for i=2, #p do
+    sum = sum + ipp.manhattan(p[i-1], p[i])
+  end
+  return sum
+end
+
+local function reverse(all, start, length)
+  local stop, ret = start + length, {}
+  print(stop)
+  for i=1,start-1 do
+    ret[i] = all[i]
+  end
+  for i=0,length do
+    ret[start + i] = all[stop - i]
+  end
+  for i=stop + 1, #all do
+    ret[i] = all[i]
+  end
+  return ret
+end
+
+function ipp.ipp(points, max)
+  max = max or 20
+  local ret = points
+  for i=1,max do
+    local start, len = math.random(#points), math.random(#points/2)
+    local cost = length(points)
+    ret = reverse(points, start, len)
+    if length(ret) < cost then
+      points = ret
+    end
+  end
+  return ret
+end
+
+return ipp
