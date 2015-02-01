1f7bafcb0c80df1e4d05aed17554c488c091d434
rmellema <rene.mellema@gmail.com>
2015-02-01 15:09:43 +0100

    Added a locateMineReturn function

--- lib/ibm.lua
+++ lib/ibm.lua
@@ -4,6 +4,12 @@ local lama = require "lama"
 
 local ibm = {}
 
+function ibm.locateMineReturn(proto, valuables, depth, range)
+  local sx, sy, sz = lama.getPosition()
+  ibm.locateAndMine(proto, valuables, depth, range)
+  lama.goTo(sx, sy, sz)
+end
+
 function ibm.locateAndMine(proto, valuables, depth, range)
   local locs = ibl.findValuables(proto, valuables, depth, range)
   local route = rpc.rpc(locs, 1000)
